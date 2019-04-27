80c6ca5fedb0d7f423200072f7a4b3374c429367
wikipockets <sg.johnson14@gmail.com>
2019-04-26 22:12:34 -0400

    backend controller routs and server

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require(mysql);
+
+var connection;
+
+// If server contains the JAWSDB_URL environmental variable, it uses the JawsDB database, otherwise it uses local MAMP
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection = mysql.createConnection({
+      host: "localhost",
+      port: 8889,
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+   });
+};
+
+
+connection.connect(function (err) {
+   if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+   }
+   console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
